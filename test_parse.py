8a4e790c0b6346f0acef6f6b502f0ae51939966c
charliekim2 <charlie2@ualberta.ca>
2024-09-03 13:38:13 -0600

    better profile

--- test_parse.py
+++ test_parse.py
@@ -4,21 +4,33 @@ import json
 
 
 def profile_parsers(filename: str) -> None:
-    with open(filename, "r") as f:
-        parser = Parser(f)
+    num_iterations = 10
+    myparser_total = 0
+    jsonload_total = 0
 
-        start = process_time()
-        parser.parse()
-        end = process_time()
+    for _ in range(num_iterations):
+        with open(filename, "r") as f:
+            parser = Parser(f)
 
-        print(f"MY PARSER: Elapsed time to process {filename}: {end - start} seconds")
+            start = process_time()
+            parser.parse()
+            end = process_time()
 
-    with open(filename, "r") as f:
-        start = process_time()
-        json.load(f)
-        end = process_time()
+            myparser_total += end - start
 
-        print(f"JSON.LOAD: Elapsed time to process {filename}: {end - start} seconds")
+        with open(filename, "r") as f:
+            start = process_time()
+            json.load(f)
+            end = process_time()
+
+            jsonload_total += end - start
+
+    print(
+        f"MY PARSER: Average time to process {filename}: {round( myparser_total / num_iterations, 4 )} seconds"
+    )
+    print(
+        f"JSON.LOAD: Average time to process {filename}: {round( jsonload_total / num_iterations, 4 )} seconds"
+    )
 
 
 if __name__ == "__main__":
